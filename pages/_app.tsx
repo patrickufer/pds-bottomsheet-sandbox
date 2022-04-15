52515388b298e1bf248a5499ba86be0ffe594584
Patrick Ufer <patrickufer18@gmail.com>
2022-04-15 14:49:19 -0400

    add PDS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { init as initMatomo } from "@socialgouv/matomo-next";
 import Head from 'next/head'
 import { useEffect, Fragment } from 'react';
 import '../styles.scss';
+import { PorscheDesignSystemProvider } from '@porsche-design-system/components-react';
 
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL + '';
 const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID + '';
@@ -15,13 +16,15 @@ export default function Application({ Component, pageProps }: AppProps) {
   }, [])
 
   return (
-      <Fragment>
-        <Head>
-          <title>Matomo PII bug + NextJS minimal repo</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        </Head>
-        <Component {...pageProps} />
-      </Fragment>
+      <PorscheDesignSystemProvider>
+        <Fragment>
+          <Head>
+            <title>Matomo PII bug + NextJS minimal repo</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+          </Head>
+          <Component {...pageProps} />
+        </Fragment>
+      </PorscheDesignSystemProvider>
     );
   
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,35 @@
+import { PButton } from '@porsche-design-system/components-react';
 import React, { Fragment, useState } from 'react';
 import { BottomSheet } from 'react-spring-bottom-sheet'
 
 const Home = (): JSX.Element => {
   const [open, setOpen] = useState(false)
+  const [blocking, setBlocking] = useState(true)
+  const [count, setCount] = useState(0)
 
   return (
   <div className="pageLayout">
     <p>Visible from Matomo</p>
     <p data-matomo-mask>Hidden from Matomo</p>
     <button onClick={() => setOpen(true)}>Open bottom sheet</button>
-    <BottomSheet open={open} header={
+    <hr/>
+    <p>Button clicked: {count.toString()} times</p>
+    <p>Blocking: {blocking.toString()}</p>
+    <button onClick={() => setBlocking(!blocking)}>Toggle BottomSheet blocking</button>
+    <BottomSheet blocking={blocking} open={open} header={
         <Fragment>
           <p>Visible by Matomo</p>
           <p data-matomo-mask>Hidden from Matomo</p>
-          <button onClick={() => setOpen(false)}>Close Sheet</button>
+          <button onClick={() => setOpen(false)}>Close Sheet</button><br/><br/>
+          <PButton onClick={() => setCount(count + 1)}>Click this PButton</PButton>
         </Fragment>
       }>
         {/* BottomSheet body */}
       <div>
         <p>Visible from Matomo</p>
         <p data-matomo-mask>Hidden from Matomo</p>
-        <button onClick={() => setOpen(false)}>Close Sheet</button>
+        <button onClick={() => setOpen(false)}>Close Sheet</button><br/><br/>
+        <PButton onClick={() => setCount(count + 1)}>Click this PButton</PButton>
       </div>
     </BottomSheet>
   </div>
