a0d6e7284b9fd75105228e189ece1f71bb53bfba
Patrick Ufer <patrickufer18@gmail.com>
2022-04-18 10:39:09 -0400

    add preconnect links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Main, Head, NextScript } from 'next/document';
 import React from 'react';
+
 class MyDocument extends Document {
 
   render() {
@@ -8,6 +9,10 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.ico" key="favicon" />  
+
+          {/* Porsche Design System fonts & styles */}
+          <link rel='preconnect' href='https://cdn.ui.porsche.com/' key='porscheui-cdn-preconnect' />
+          <link rel='preconnect' href='https://aws.designsystem.porsche.com' key='porscheui-aws-preconnect' />
         </Head>
         <body>
           <Main />
